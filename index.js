ad38a2422df0adfc283e4222ccef0e8c750e90ec
DiegoVarzim <diego.varzim@hotmail.com>
2023-03-21 20:10:06 -0300

    atualizações do dia 21/03/23

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log('Agora eu sou Desenvolvedor');
+console.log('Javascript Funcionando'); 
+
+
+let firstName = 'Diego';
+let lastName ='Varzim'
+console.log(firstName);
+console.log(lastName);
+
+let color = 'Grey';
+console.log(color);
+
+console.log('Diego de Oliveira Varzim da Silva');
+console.log('Agora eu sou JavaScript Engineer');
+console.log('Agora é seguir em frente e não desistir do projeto!!');
+console.log('FOCO!!!!');
+
 
