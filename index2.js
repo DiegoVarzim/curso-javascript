06852007a05a36f675e696eb9ecb8f669126152e
DiegoVarzim <diego.varzim@hotmail.com>
2023-03-21 20:47:55 -0300

    ok

---
+++ index2.js
@@ -0,0 +1,19 @@
+console.log('Testando...');
+
+const price = '30';
+// price = '20';
+console.log(price);
+
+
+let itemName = 'Pen';
+let itemPrice = 14;
+let itemAvailable = true;
+let itemColor;
+itemName = null; // trocando a variável itemName p/ null.
+
+console.log(itemName);
+
+
+
+
+
