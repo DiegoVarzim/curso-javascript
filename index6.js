a3ebabb15ed1c987c7c588109009e1cc0bc5d6da
Diego Varzim <diego.varzim@hotmail.com>
2023-03-23 10:02:11 -0300

    atualizações do dia 23/03/23

---
+++ index6.js
@@ -0,0 +1,15 @@
+
+num1 = 3
+
+// num1 = num1 + 20
+num1 /= 20 // operadora de atribuição
+
+console.log(num1)
+
+// % resto da divisão
+// ** exponenciação
+
+// ++ incremento é feito mais um
+// -- decremento
+
+
---
+++ index7.js
@@ -0,0 +1,14 @@
+let num1 = 'Diego'
+
+// console.log(num1 < 0)
+// console.log(num1 <= 5)
+// console.log(num1 > 0)
+// console.log(num1 >= 8)
+
+console.log(num1 !== 'Diego')
+
+
+// === é igual
+// === não é igual
+
+
